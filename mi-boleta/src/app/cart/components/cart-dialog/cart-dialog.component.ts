298e5e99531112e08344bb80351465611bedae94
esteban2505J <juane.ramirezt@uqvirtual.edu.co>
2024-11-08 23:18:41 -0500

    feat(shopping): the view of the purchases was added

--- mi-boleta/src/app/cart/components/cart-dialog/cart-dialog.component.ts
+++ mi-boleta/src/app/cart/components/cart-dialog/cart-dialog.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import { SidebarModule } from 'primeng/sidebar';
-import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-cart-dialog',
   standalone: true,
-  imports: [SidebarModule, ButtonModule],
+  imports: [],
   templateUrl: './cart-dialog.component.html',
   styleUrl: './cart-dialog.component.css'
 })
 export class CartDialogComponent {
-  sidebarVisible: boolean = false;
-
+ 
 }

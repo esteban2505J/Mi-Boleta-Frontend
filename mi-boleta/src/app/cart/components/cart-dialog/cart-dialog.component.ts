cb3f7f968e9dcfeb9f970f47bfd9c05018eb68d3
esteban2505J <juane.ramirezt@uqvirtual.edu.co>
2024-11-10 23:28:00 -0500

    feat(cart): customized the shopping cart section

--- mi-boleta/src/app/cart/components/cart-dialog/cart-dialog.component.ts
+++ mi-boleta/src/app/cart/components/cart-dialog/cart-dialog.component.ts
@@ -1,13 +1,20 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-cart-dialog',
   standalone: true,
-  imports: [],
+  imports: [NgIf,NgClass],
   templateUrl: './cart-dialog.component.html',
   styleUrl: './cart-dialog.component.css'
 })
 export class CartDialogComponent {
+
+  selectedTab: string = 'active';
+
+  selectTab(tab: string) {
+    this.selectedTab = tab;
+  }
  
 }
